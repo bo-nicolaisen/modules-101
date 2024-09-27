13d8273629d45da1789af6cc3b349bd265297fc7
bo-nicolaisen <bni@techcollege.dk>
2024-09-27 11:07:16 +0200

    ready

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,11 +1,3 @@
 // write cool JS hwere!!
 
-import createClock from './modules/clock/clock.js';
-import createCounter from './modules/counter/counter.js';
-import randomDog from './modules/dog/randomDog.js';
 
-
-createClock("clock")
-createCounter("count", 0)
-
-randomDog("dog")
