41f7cdb84e8547f1074ea2a7ced03414779f7096
bo-nicolaisen <bni@techcollege.dk>
2024-09-27 12:51:13 +0200

    bob

--- assets/js/modules/mealDB/mealDB_model.js
+++ assets/js/modules/mealDB/mealDB_model.js
@@ -3,19 +3,28 @@ export default async function getAllProducts() {
 
 }
 
+export async function getCategories() {
+    let categoryURL = 'https://www.themealdb.com/api/json/v1/1/categories.php'
+
+}
+
 export async function findProduct(searchString) {
-    console.log(searchString);
+
 
     let searchURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`
 
-    return fetch(searchURL)
+    return fetchStuff(searchURL)
+}
+
+
+async function fetchStuff(url) {
+
+    return fetch(url)
         .then((response) => {
 
             if (!response.ok) {
                 throw new Error('Network response was not ok')
             }
-
-
             return response.json()
         })
         .then(data => {
